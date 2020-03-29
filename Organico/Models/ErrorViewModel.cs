0cae19998cafbbd2758a59ef8565cf82edc24202
JuanCarlos090897 <36517516+JuanCarlos090897@users.noreply.github.com>
2020-03-28 19:11:43 -0600

    Creacion del proyecto con ignoracion de archivo para poder subirlo a github

---
+++ Organico/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Organico.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
